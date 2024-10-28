a3a9d9738636a937e0e34d130c70daf31c6ec911
nradin984 <natasa.tefter@gmail.com>
2024-10-28 10:14:40 +0100

    js

---
+++ JS/controle-flow.js
@@ -0,0 +1,5 @@
+function maxNum(num1,num2){
+    return num1>=num2 ? num1 : num2;
+}
+
+console.log(maxNum(2,1));
--- JS/variables1.js
+++
@@ -1,5 +0,0 @@
-let productColors=['blue,' 'green'];
-productColors(0)=42;
-console.log(productColors.length);
-
-
